9a13ffa42a7053c50a84c430106addee6b5a4620
BatyaBracha <Hand4d2@mbjcomp.org.il>
2025-05-13 16:26:02 +0300

    Refactor database connection to use environment variables and update HTML title

--- Server/DB/createTables.js
+++ Server/DB/createTables.js
@@ -1,12 +1,13 @@
 // אחראי על יצירת הטבלאות בדאטהבייס בלבד, פעם אחת בלבד בפרויקט
 const mysql = require('mysql2');
 const tables = require('../MVC/models/tables_definition');
+const details=require('./details.env');
 
 const connection = mysql.createConnection({
-    user: 'root',
-    password: 'BatyaOren20',
-    database: 'project',
-    port: '3306'
+    user: details.USER,
+    password: details.PASSWORD,
+    database: details.DB,
+    port: details.PORT
 });
 
 Object.entries(tables).forEach(([name, query]) => {
